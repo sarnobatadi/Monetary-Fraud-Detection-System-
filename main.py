4f31b5c9d700c185d6a179bbed41332151144206
Aditya <sarnobatadi@gmail.com>
2022-11-19 15:44:33 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,354 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+from sklearn.metrics import average_precision_score
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+
+# Commented out IPython magic to ensure Python compatibility.
+# importing the dataset using dask
+
+data = pd.read_csv('fraud_data.csv')
+print(data.shape)
+
+# getting the information related to data
+
+# data.info()
+
+#describing the data
+
+# data.describe()
+
+#checking how many frauds are there
+
+# data['isFraud'].value_counts()
+
+labels = ['Not a Fraud', 'Fraud']
+colors = ['pink', 'black']
+size = [6354407, 8213]
+explode = [0.1, 0.21]
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.pie(size, labels = labels, colors = colors, explode = explode, shadow = True)
+plt.axis('off')
+plt.title('A pie chart representing share of frauds amongst the customers',fontsize = 20)
+plt.legend()
+plt.show()
+
+# data['isFlaggedFraud'].value_counts()
+
+labels = ['Not a Flagged Fraud', 'Flagged Fraud']
+colors = ['lightblue', 'black']
+size = [6354407, 8213]
+explode = [0.1, 0.21]
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.pie(size, labels = labels, colors = colors, explode = explode, shadow = True)
+plt.axis('off')
+plt.title('A pie chart representing share of flagged frauds amongst the customers',fontsize = 20)
+plt.legend()
+plt.show()
+
+# data['type'].value_counts()
+
+labels = ['Cash-out', 'Payment', 'Cash-in', 'Transfer', 'Debit']
+size = [2237500, 2151495, 1399284, 532909, 41432]
+colors = ['pink', 'lightgreen', 'lightblue', 'orange', 'yellow']
+explode = [0.1, 0.1, 0.1, 0.1, 0.1]
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.pie(size, colors = colors, explode = explode, labels = labels, shadow = True)
+plt.title('A pie chart representing different types of money transactions', fontsize = 20)
+plt.axis('off')
+plt.legend()
+plt.show()
+
+# checking the different types of steps involved in transactions
+
+x = data['step'].value_counts().nunique()
+
+# printing the result
+print("The no. of different steps involved in transactions are :", x)
+
+# setting thesze of the figure
+plt.rcParams['figure.figsize'] =(14, 7)
+
+# importing norm
+
+sns.distplot(data.step, fit = norm, kde = False)
+plt.title('Distribution Plot for steps', fontsize = 20)
+plt.xlabel('Step', fontsize = 10)
+plt.show()
+
+# checking the different values of amounts transationed
+
+plt.rcParams['figure.figsize'] = (10, 7)
+
+# data['amount'].value_counts().head(15).plot.bar()
+plt.title('15 Most Common Transaction amounts', fontsize = 20)
+plt.xlabel('Amounts')
+plt.ylabel('count')
+plt.show()
+
+# checking the different values of amounts transationed
+
+plt.rcParams['figure.figsize'] = (19, 7)
+
+# data['amount'].value_counts().tail(50).plot.bar()
+plt.title('50 Least Common Transaction amounts', fontsize = 20)
+plt.xlabel('Amounts')
+plt.ylabel('count')
+plt.show()
+
+# setting thesze of the figure
+plt.rcParams['figure.figsize'] =(14, 7)
+
+# setting the style in seaborn
+sns.set(style = 'dark', palette = 'colorblind')
+
+#plotting a dist plot
+# ax = sns.distplot(data.amount)
+# ax.set_title(label = 'Distribution Plot for amount', fontsize = 20)
+# ax.set_xlabel(xlabel = 'Amount', fontsize = 10)
+# plt.show()
+
+# finding  out the types of fraudulent transaction types out of all the transaction modes.
+
+print('The Types of fraudulent transactions are {}'.format(list(data.loc[data.isFraud == 1].type.drop_duplicates().values)) )
+
+# figuring out the fraud transfer and fraud cash outs
+dfFraudTransfer = data.loc[(data.isFraud == 1) & (data.type == 'TRANSFER')]
+dfFraudCashout = data.loc[(data.isFraud == 1) & (data.type == 'CASH_OUT')]
+
+
+#printing the number of fraudulent transfers ans cash outs
+print ('The number of fraudulent TRANSFERs = {}'.format(len(dfFraudTransfer)))
+print ('The number of fraudulent CASH_OUTs = {}'.format(len(dfFraudCashout)))
+
+# finding out the minimum and maximum transaction from Transfer and cash out transaction mode when it is set to be Fraud
+
+print("Transfer Transaction")
+print("Minimum Transaction :", data.loc[(data.isFraud == 1) & (data.type == 'TRANSFER')].amount.min())
+print("Maximum Transaction :", data.loc[(data.isFraud == 1) & (data.type == 'TRANSFER')].amount.max())
+
+print("\nCASH_OUT Transaction")
+print("Minimum Transaction :", data.loc[(data.isFraud == 1) & (data.type == 'CASH_OUT')].amount.min())
+print("Maximum Transaction :", data.loc[(data.isFraud == 1) & (data.type == 'CASH_OUT')].amount.max())
+
+#finding out the transactions which are flagged to be fraud
+
+print("The Type of Transaction in which IsFlagged is set :", data.loc[data.isFlaggedFraud == 1].type.drop_duplicates().values)
+
+# figuringing out the no. of flagged fraudulent transactions
+FlaggedFraud = data.loc[(data.isFlaggedFraud == 1) & (data.type == 'TRANSFER')]
+print("The no. of Flagged Fraudulent Transactions :", len(FlaggedFraud))
+
+# printing the minimum and maximum transactions done when FlaggedFraud is set
+# the Transaction mode being Transfer
+print("Minimum Transaction :", data.loc[data.isFlaggedFraud == 1].amount.min())
+print("Maximum Transaction :", data.loc[data.isFlaggedFraud == 1].amount.max())
+
+# Figuring out the transactions where old balance dest. and new balance dest. are 0 being FlaggedFraud
+
+x = data.loc[(data.isFlaggedFraud == 0) & (data.oldbalanceDest == 0) & (data.newbalanceDest == 0)]
+print("Figuring out the no. of transactions where old balance dest. and new balance dest. are 0 being FlaggedFraud :", x)
+
+"""**Dataset where only Transfer Transactions are mentioned**"""
+
+# defining a dataset containing only transfer type of transactions
+
+dataTransfer = data.loc[data['type'] == 'TRANSFER']
+
+dataTransfer = pd.DataFrame(dataTransfer)
+
+dataTransfer.head(10)
+
+"""**Minimum Original old Balance for Flagged Frauds**"""
+
+# frauds having same old and new balance
+# sorted by old orignal balance to check original old balance for flagged frauds
+
+# this table shows flagged frauds with minimum original old balance
+
+
+dataTransfer.loc[(dataTransfer.isFlaggedFraud == 1) & (dataTransfer.oldbalanceOrg == dataTransfer.newbalanceOrig)].sort_values(by = 'oldbalanceOrg').head(10)
+
+"""**Maximum Original Balance for Flagged Frauds**"""
+
+# frauds having same old and new balance
+# sorted by old orignal balance to check original old balance for flagged frauds
+
+# this table shows flagged frauds with maximum original old balance
+
+
+dataTransfer.loc[(dataTransfer.isFlaggedFraud == 1) & (dataTransfer.oldbalanceOrg == dataTransfer.newbalanceOrig)].sort_values(by = 'oldbalanceOrg').tail(10)
+
+"""**Minimum Original Old Balace for non-Flagged Frauds**"""
+
+dataTransfer.loc[(dataTransfer['isFlaggedFraud'] == 0) & (dataTransfer.oldbalanceDest == dataTransfer.newbalanceDest)].sort_values(by = 'oldbalanceOrg').head(10)
+
+"""**Maximum Original Old Balance for Non-Flagged Frauds**"""
+
+dataTransfer.loc[(dataTransfer['isFlaggedFraud'] == 0) & (dataTransfer.oldbalanceDest == dataTransfer.newbalanceDest)].sort_values(by = 'oldbalanceOrg').tail(10)
+
+# finding out the minimum and maximum amount for oldbalance original where oldbalance and newbalance for dest is same.
+
+dataFlagged = data.loc[data.isFlaggedFraud == 1]
+
+print('Minimum Balance of oldBalanceOrig for FlaggedFraud and Transfer mode :', dataFlagged.oldbalanceOrg.min())
+print('Maximum Balance of oldbalanceOrig for FlaggedFraud and Transfer mode :', dataFlagged.oldbalanceOrg.max())
+
+# Finding the maximum and minimum balance in original balance when newbalance original and oldbalance original are equal
+
+print('Minimum Balance of oldbalanceOrig for non FlaggedFraud and Transfer mode:',
+      data[(data.type == 'TRANSFER') & (data.isFlaggedFraud == 0) & (data.oldbalanceOrg == data.newbalanceOrig)].oldbalanceOrg.min())
+print('Maximum Balance of oldbalanceOrig for non FlaggedFraud and Transfer mode:',
+      data[(data.type == 'TRANSFER') & (data.isFlaggedFraud == 0) & (data.oldbalanceOrg == data.newbalanceOrig)].oldbalanceOrg.max())
+
+# Can isFlaggedFraud be set based on seeing a customer transacting more than once? Note that duplicate customer names don't exist within 
+# transactions where isFlaggedFraud is set, but duplicate customer names exist within transactions where isFlaggedFraud is not set. It turns 
+# out that originators of transactions that have isFlaggedFraud set have transacted only once. Very few destination accounts of transactions 
+# that have isFlaggedFraud set have transacted more than once.
+
+print('Minimum Balance of oldbalanceOrig for  FlaggedFraud and Transfer mode:',
+      data[(data.type == 'TRANSFER') & (data.isFlaggedFraud == 1) & (data.oldbalanceOrg == data.newbalanceOrig)].oldbalanceOrg.min())
+print('Maximum Balance of oldbalanceOrig for non FlaggedFraud and Transfer mode:',
+      data[(data.type == 'TRANSFER') & (data.isFlaggedFraud == 1) & (data.oldbalanceOrg == data.newbalanceOrig)].oldbalanceOrg.max())
+
+data['nameDest'].value_counts().head(20)
+
+# making dependent and independent sets
+
+X = data.loc[(data.type == 'TRANSFER') | (data.type == 'CASH_OUT')]
+
+# shape of x
+X.shape
+
+X.head()
+
+# feature engg.
+
+X['errorBalanceOrig'] = X.newbalanceOrig + X.amount - X.oldbalanceOrg
+X['errorBalanceDest'] = X.oldbalanceDest + X.amount - X.newbalanceDest
+
+# removing unwanted columns
+
+X = X.drop(['nameDest','nameOrig'], axis = 1)
+
+# checking the new shape of data
+X.shape
+
+# label encoding for the transaction type
+
+X['type'].replace('TRANSFER', 0, inplace = True)
+X['type'].replace('CASH_OUT', 1, inplace = True)
+
+# looking at the head of the data
+
+X.head(10)
+
+# checking if the dataset contains any NULL values
+
+X.isnull().any().any()
+
+# visualizing the distribution of errorbalance
+
+# setting the size of the plot
+# plt.rcParams['figure.figsize'] = (21, 7)
+
+# plt.subplot(121)
+# sns.distplot(X['errorBalanceOrig'], color = 'red')
+# plt.title('Error Balance of Origin', fontsize = 20)
+
+# plt.subplot(122)
+# sns.distplot(X['errorBalanceDest'], color = 'red')
+# plt.title('Error Balance of Destination', fontsize = 20)
+
+# plt.show()
+
+# checking the no. of frauds in the dataset
+
+# X['isFraud'].Fvalue_counts()
+
+# plotting a pie chart to represent the fraction of the frauds 
+
+size = [2762196, 8213]
+labels = ['NON-FRAUD', 'FRAUD']
+colors = ['lightblue', 'yellow']
+explode = [0, 0.2]
+
+plt.pie(size, labels = labels, colors = colors, explode = explode, shadow  = True, autopct = '%.2f%%')
+plt.title('Frauds v/s Non-Frauds', fontsize = 20)
+plt.axis('off')
+plt.legend()
+plt.show()
+
+X['isFraud'].value_counts()
+
+# making our independent set
+
+Y = X['isFraud']
+
+# removing the dependent set
+X = X.drop(['isFraud'], axis = 1)
+
+# getting the shapes of x and y
+print("Shape of x: ", X.shape)
+print("Shape of y: ", Y.shape)
+
+X.head()
+
+"""**OVER-SAMPLING**"""
+
+x_resample, y_resample = SMOTE().fit_resample(X, Y.values.ravel())
+
+# getting the shapes of x and y after resampling
+print("Shape of x: ", x_resample.shape)
+print("Shape of y:", y_resample.shape)
+
+# splitting the dataset into train and tests
+
+
+x_train, x_test, y_train, y_test = train_test_split(x_resample, y_resample, test_size = 0.2, random_state = 0)
+
+# checking the new shapes
+print("Shape of x_train: ", x_train.shape)
+print("Shape of x_test: ", x_test.shape)
+print("Shape of y_train: ", y_train.shape)
+print("Shape of y_test: ", y_test.shape)
+
+# performing standard scaling on the data for better fit
+
+
+sc = StandardScaler()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+
+
+model = XGBClassifier()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+# score of the model
+auprc = average_precision_score(y_test, y_pred)
+print("The Area under Precision Recall Curve Score is", auprc)
+
+# looking at the confusion matrix
+
+
+
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
+
